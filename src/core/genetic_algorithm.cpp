837e8d04706ca9ed421869f851bd654e71240970
Jujuman-Home <jussmith48@gmail.com>
2015-06-17 16:08:04 -0400

    Some Changes

--- src/core/genetic_algorithm.cpp
+++ src/core/genetic_algorithm.cpp
@@ -31,7 +31,7 @@ Last modified by: Justin Smith
 1)Creates Oldest Ancestor Gene (OAG)
 2)Produces initial population N from (OAG)
 3)Obtain target excited energies and ground
-  state charges. TESTING
+  state charges.
 -------------------------------------------*/
 void GeneticAlgorithm::GeneticAlgorithmPreparation()
 {
